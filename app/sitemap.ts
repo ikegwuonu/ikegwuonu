df5716e981e7542db2e02076eddbd0e808b25dd9
ikegwuonu <juliet.kegwuonu@gmail.com>
2026-02-08 20:04:10 +0100

    updated path in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://ikegwuonu.vercel.app/";
+  const baseUrl = "https://ikegwuonu.vercel.app";
 
   return [
     {
